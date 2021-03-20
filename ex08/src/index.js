b0f2acf24abc0a66aaf706f8682aeb37ad9d1eaf
EmilJulevic <emiljulevic@live.com>
2021-03-20 09:41:30 +0100

    Adding folders

---
+++ ex08/src/index.js
@@ -0,0 +1,49 @@
+var contacts = [
+    {
+        "firstName": "John",
+        "lastName": "Doe",
+        "number": "12365495208",
+        "likes": ["C", "C++", "C#"]
+    },
+    {
+        "firstName": "Jane",
+        "lastName": "Doe",
+        "number": "4568295761",
+        "likes": ["Python", "Swift", "R"]
+    },
+    {
+        "firstName": "Richard",
+        "lastName": "Roe",
+        "number": "4568295761",
+        "likes": ["React", "Angular", "Vue"]
+    },
+    {
+        "firstName": "Jane",
+        "lastName": "Roe",
+        "number": "unknown",
+        "likes": ["JavaScript", "Node", "HTML & CSS"]
+    }
+];
+
+function lookUpProfile(name, prop) {
+
+    for (var i = 0; i < contacts.length; i += 1) {
+        var contact = contacts[i];
+        if (contact.firstName === name) {
+            if (contact[prop] != undefined) {
+                return contact[prop];
+            }
+            return "No such property";
+        }
+    }
+    return "No such contact";
+}
+
+
+console.log(lookUpProfile("John", "likes"));
+console.log(lookUpProfile("Jane", "lastName"));
+console.log(lookUpProfile("Richard", "likes"));
+console.log(lookUpProfile("Rob", "number"));
+console.log(lookUpProfile("Rob", "JavaScript"));
+console.log(lookUpProfile("John", "address"));
+module.exports = lookUpProfile;
